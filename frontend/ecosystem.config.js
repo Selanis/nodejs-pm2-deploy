ff0c05bdd932f3edbc942a30a4e47d4b7c2de1c5
Selanis <k.polexowa@yandex.ru>
2025-11-06 14:57:35 +0300

    Удалила из конфига фронта копиирование env файлов на сервер

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,6 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: REPOSITORY_PATH,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd frontend && pwd && npm i && npm run build',
     },
   },
