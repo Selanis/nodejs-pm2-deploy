3ec79b909f8973a745ed6b7564bf1a2b61202b6d
Selanis <k.polexowa@yandex.ru>
2025-11-06 13:01:23 +0300

    ..

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: REPOSITORY_PATH,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'NODE_OPTIONS=--openssl-legacy-provider react-scripts build',
+      'post-deploy': 'npm i && react-scripts --openssl-legacy-provider build',
     },
   },
 }
